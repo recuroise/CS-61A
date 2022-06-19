05cf022926ef822b4b8434d87742e7bb5b0668ee
recuroise <2430681986@qq.com>
2022-06-19 22:31:57 +0800

    Delete lab00 directory

--- lab00/lab00.py
+++
@@ -1,9 +0,0 @@
-def twenty_twenty():
-    """Come up with the most creative expression that evaluates to 2020,
-    using only numbers and the +, *, and - operators.
-
-    >>> twenty_twenty()
-    2020
-    """
-    return 114+514+114+514+114+514+114+514-11*45-1-4-1+1*4+5
-
