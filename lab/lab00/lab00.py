288ada911374b82f8e099d5d019059e57bc7b077
recuroise <2430681986@qq.com>
2022-06-19 22:31:36 +0800

    Add files via upload

---
+++ lab/lab00/lab00.py
@@ -0,0 +1,9 @@
+def twenty_twenty():
+    """Come up with the most creative expression that evaluates to 2020,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty()
+    2020
+    """
+    return 114+514+114+514+114+514+114+514-11*45-1-4-1+1*4+5
+
