73038619785ea72d883bb083d8c1100e59d28ce0
recuroise <2430681986@qq.com>
2022-07-15 09:59:07 +0800

    Add files via upload

---
+++ hw/hw05/tempCodeRunnerFile.py
@@ -0,0 +1,29 @@
+def merge(incr_a, incr_b):
+    """Yield the elements of strictly increasing iterables incr_a and incr_b, removing
+    repeats. Assume that incr_a and incr_b have no repeats. incr_a or incr_b may be infinite
+    sequences.
+
+    >>> m = merge([0, 2, 4, 6, 8, 10, 12, 14], [0, 3, 6, 9, 12, 15])
+    >>> type(m)
+    <class 'generator'>
+    >>> list(m)
+    [0, 2, 3, 4, 6, 8, 9, 10, 12, 14, 15]
+    >>> def big(n):
+    ...    k = 0
+    ...    while True: yield k; k += n
+    >>> m = merge(big(2), big(3))
+    >>> [next(m) for _ in range(11)]
+    [0, 2, 3, 4, 6, 8, 9, 10, 12, 14, 15]
+    """
+    iter_a, iter_b = iter(incr_a), iter(incr_b)
+    next_a, next_b = next(iter_a, None), next(iter_b, None)
+    "*** YOUR CODE HERE ***"
+    while next_a and next_b:
+        if next_a < next_b:
+            yield next_a
+            next_a = next(iter_a, None)
+        else:
+            yield next_b
+            next_b = next(iter_b, None)
+
+m = merge([0, 2, 4, 6, 8, 10, 12, 14], [0, 3, 6, 9, 12, 15])
